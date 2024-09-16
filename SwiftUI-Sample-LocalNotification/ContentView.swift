28f4e3d4500a85983bac44056a5fe588ceec7673
NanbanTaro <>
2024-09-16 13:07:25 +0900

    Create New Project

---
+++ SwiftUI-Sample-LocalNotification/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI-Sample-LocalNotification
+//
+//  Created by NanbanTaro on 2024/09/16.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Sample-LocalNotification/SwiftUI_Sample_LocalNotificationApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_Sample_LocalNotificationApp.swift
+//  SwiftUI-Sample-LocalNotification
+//
+//  Created by NanbanTaro on 2024/09/16.
+//  
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sample_LocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
